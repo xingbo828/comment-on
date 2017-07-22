4cca3b32832cf438d42144796022e8da9b49baa5
Bo Xing <xingbo828@gmail.com>
2017-07-22 13:00:14 -0700

    add upload profile pic route

--- src/globalComponents/Form/index.js
+++ src/globalComponents/Form/index.js
@@ -1,9 +1,11 @@
 import TextField from './TextField';
 import Radio from './Radio';
 import RadioGroup from './RadioGroup';
+import Upload from './Upload';
 
 export {
   TextField,
   Radio,
-  RadioGroup
+  RadioGroup,
+  Upload
 };
--- src/modules/Account/index.js
+++ src/modules/Account/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Switch, Link } from 'react-router-dom';
 import ProtectedRoute from '../Common/ProtectedRoute';
 import BasicProfile from './BasicProfileContainer';
+import ProfilePicture from './ProfilePictureContainer';
 
 const Account = () => {
   return (
     <div>
       <ul>
         <li><Link to="/account/profile-basic">Manage my basic profile</Link></li>
+        <li><Link to="/account/profile-picture">Manage my profile picture</Link></li>
       </ul>
       <Switch>
         <ProtectedRoute path="/account/profile-basic" component={BasicProfile} />
+        <ProtectedRoute path="/account/profile-picture" component={ProfilePicture} />
       </Switch>
     </div>
   );
