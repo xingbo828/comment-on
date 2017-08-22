b7679b60a4e45472baf6aeccfd8838c243d00bf0
William Chen <deviozc@gmail.com>
2017-08-21 21:45:08 -0700

    Add cloud_functions

---
+++ cloud_functions/functions/index.js
@@ -0,0 +1,117 @@
+'use strict';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyDJjuf5Cli-bjDPh_lWLhPoSqy7UPCr0xM',
+    Promise: Promise
+});
+const moment = require('moment');
+const cors = require('cors')({origin: true});
+
+admin.initializeApp(functions.config().firebase);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+exports.hellowWorld = functions.https.onRequest((request, response) => {
+    response.send("hello~");
+});
+
+const extractCity = (placeData) => {
+    const location = {};
+    if (!placeData) {
+        return;
+    }
+    placeData.json.result.address_components.some((addressComponent) => {
+        if (addressComponent.types[1] === 'political') {
+            if (addressComponent.types[0] === 'locality') {
+                location.city = addressComponent.short_name.toLowerCase();
+            } else if (addressComponent.types[0] === 'administrative_area_level_1') {
+                location.province = addressComponent.short_name.toLowerCase();
+            }
+        }
+        return location.city && location.province;
+    });
+    return `${location.province}-${location.city}`;
+}
+
+exports.business = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        business(request, response);
+    });
+})
+
+const filterByDestination = (destinationCity) => (business) => {
+    if (!destinationCity) {
+        return true;
+    }
+    return business.businessServiceArea[destinationCity]
+};
+
+const filterByDateTime = (dateTime) => (business) => {
+    if (!dateTime) {
+        return true;
+    }
+    const dateTimeObj = moment.unix(dateTime);
+    const day = dateTimeObj.format('ddd').toLowerCase();
+    const hour = dateTimeObj.hours() + dateTimeObj.minutes()/60;
+    return business.businessHour.some((businessHour) => {
+        return businessHour.day === day && businessHour.startTime <= hour && businessHour.endTime >= hour;
+    });
+};
+
+const business = ((request, response) => {
+    const origin = request.query.origin;
+    const destination = request.query.destination;
+    const dateTime = request.query.dateTime;
+
+    let destinationCity;
+
+    if (!origin) {
+        return response.status(400).json({error: 'Empty origin'});
+    }
+
+    Promise.all([googleMapsClient.place({placeid: origin}).asPromise(),
+        destination ? googleMapsClient.place({placeid: destination}).asPromise() : Promise.resolve()])
+        .then((data) => {
+            const originCity = extractCity(data[0]);
+            destinationCity = extractCity(data[1]);
+            return originCity;
+        })
+        .then((location) => {
+            return admin.database().ref('/businesses')
+                .orderByChild(`businessServiceArea/${location}`).equalTo(true)
+                .once('value')
+        })
+        .then((value) => {
+            const businessData = value.val();
+            if (!businessData) {
+                return response.status(404).json([]);
+            }
+            let businesses = Object.keys(businessData)
+                .map((businessId) => {
+                    const result = businessData[businessId];
+                    result.id = businessId;
+                    return result;
+                });
+            if (destinationCity || dateTime) {
+                const cityFilter = filterByDestination(destinationCity);
+                const dateFilter = filterByDateTime(dateTime);
+                businesses = businesses.filter((business) => {
+                    return cityFilter(business) && dateFilter(business);
+                });
+            }
+
+            response.json(businesses);
+        })
+
+    // admin.database().ref('/businesses')
+    //     .orderByChild('businessServiceArea/bc-vancouver').equalTo(true)
+    //     .once('value').then((value) => {
+
+    //         response.json(value.val());
+    //     });
+});
