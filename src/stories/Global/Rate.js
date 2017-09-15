296e4b431b094d12f6c700282699e1271395735c
Bo Xing <xingbo828@gmail.com>
2017-09-15 11:02:06 -0700

    finish Rate component

--- src/stories/Global/Rate.js
+++ src/stories/Global/Rate.js
@@ -2,17 +2,36 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import Rate from '../../globalComponents/Rate';
 
-const RateDemo = withInfo('Badge as a dot')(() =>
+const RateDemo = withInfo('Default Rate')(() =>
   <div style={{padding: '25px'}}>
-    <Rate />
+    <Rate
+      value={2.5}
+      onChange={action('Rate changed')}
+      readOnly={boolean('readonly', false)}
+    />
+  </div>
+);
+
+const RateWithCustomIcon = withInfo('Rate with custom icon')(() =>
+  <div style={{padding: '25px'}}>
+    <Rate
+      icon="heart"
+      size="lg"
+      value={3.5}
+      onChange={action('Rate changed')}
+      readOnly={false}
+    />
   </div>
 );
 
 
 const RateStory = storiesOf('Global/Rate', module)
 .addDecorator(withKnobs)
-.add('Usage', RateDemo);
+.add('Basic rate', RateDemo)
+.add('Rate with custom icon', RateWithCustomIcon);
+
 export default RateStory;
