1a6f234d5e3135f4623b39d720d446391a999f60
Bo Xing <xingbo828@gmail.com>
2018-01-20 14:53:32 -0800

    update

--- src/modules/Project/Overview/MoveCard/index.js
+++ src/modules/Project/Overview/MoveCard/index.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import Card from '../../../../globalComponents/Card';
 import Map from '../../../../globalComponents/Map';
-import Button from '../../../../globalComponents/Form/Button';
+import Badge from '../../../../globalComponents/Badge';
 import Grid from '../../../../globalComponents/Grid';
 import {CardContainer} from './Styled';
 
 const MoveCard = ({ project }) => {
-  const withDirection = [{
-    lat: 49.2812054,
-    lng: -123.11837050000003
-  },
-  {
-    lat: 49.204811,
-    lng: -122.90685289999999
-  }];
+  const { addresses: { pickUpAddress, deliveryAddress } } = project.configuration;
+  const direction = [
+    {
+      placeId: pickUpAddress
+
+    },
+    {
+      placeId: deliveryAddress
+    }
+  ];
   return (
-    <CardContainer xs={24} sm={24} md={8} lg={8}>
-      <Card
-        primaryAction={<Button small>Click</Button>}
-      >
+    <Card>
+      <Badge count={7}>
           <Map
             style={{ height: 225, width: '100%' }}
             google={window.google}
-            markers={withDirection}
-            zoom={12}
-            mapOptions={{draggable: false, fullscreenControl: false }}
+            markers={direction}
+            mapOptions={{
+              mapTypeControl: false,
+              draggable: false,
+              fullscreenControl: false,
+              streetViewControl: false,
+              zoomControl: false
+            }}
             direction
           />
+          </Badge>
       </Card>
-    </CardContainer>
   );
 };
 
