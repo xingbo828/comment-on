8ae9b50a6d8e402406f7fefb22ae95f9750c3aac
Bo Xing <xingbo828@gmail.com>
2018-01-20 13:41:14 -0800

    add in overviewcard

--- src/globalComponents/ProgressPanels/Styled.js
+++ src/globalComponents/ProgressPanels/Styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const ProgressPanelsContainer = styled.ul`
   padding: 0;
+  margin: 0;
   border-radius: 3px;
   background-color: white;
   box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.07);
---
+++ src/modules/Project/Management/Move/OverviewCard/OverviewCard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import moment from 'moment';
+import Card from '../../../../../globalComponents/Card';
+import Map from '../../../../../globalComponents/Map';
+import Icon from '../../../../../globalComponents/Icon';
+import PlaceIdToAddress from '../../../../../globalComponents/GooglePlaceIdToAddress';
+import {
+  OverviewCardContainer,
+  OverviewCardMeta,
+  OverviewCardMetaItem,
+  OverviewCardMetaItemIcon
+} from './Styled';
+
+const OverviewCard = ({ configuration }) => {
+  console.log(configuration)
+  const { addresses: { pickUpAddress, deliveryAddress }, dateTime: { pickUpDate } } = configuration;
+  const pickUpDateMoment = moment(pickUpDate);
+  const direction = [
+    {
+      placeId: pickUpAddress
+
+    },
+    {
+      placeId: deliveryAddress
+    }
+  ];
+  return (
+    <OverviewCardContainer>
+      <Card>
+        <Map
+          style={{ height: 225, width: '100%' }}
+          google={window.google}
+          markers={direction}
+          mapOptions={{
+            mapTypeControl: false,
+            draggable: false,
+            fullscreenControl: false,
+            streetViewControl: false,
+            zoomControl: false
+          }}
+          direction
+        />
+        <OverviewCardMeta>
+          <OverviewCardMetaItem>
+            <OverviewCardMetaItemIcon>
+              <Icon icon="circle-o" />
+            </OverviewCardMetaItemIcon>
+            <PlaceIdToAddress placeId={pickUpAddress} google={window.google}/>
+          </OverviewCardMetaItem>
+          <OverviewCardMetaItem>
+            <OverviewCardMetaItemIcon>
+              <Icon icon="map-marker" />
+            </OverviewCardMetaItemIcon>
+            <PlaceIdToAddress placeId={deliveryAddress} google={window.google}/>
+          </OverviewCardMetaItem>
+          <OverviewCardMetaItem>
+            <OverviewCardMetaItemIcon>
+              <Icon icon="calendar" />
+            </OverviewCardMetaItemIcon>
+            { pickUpDateMoment.format('MMMM, DD, Y') }
+          </OverviewCardMetaItem>
+        </OverviewCardMeta>
+      </Card>
+    </OverviewCardContainer>
+  );
+};
+
+export default OverviewCard;
