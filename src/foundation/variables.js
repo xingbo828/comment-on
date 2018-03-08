4e1c2db93eda5fc841b09b77363176ff87b10d67
Nathan Marshall <nathan123ca@gmail.com>
2018-03-07 16:30:08 -0800

    resolving merge

--- src/foundation/variables.js
+++ src/foundation/variables.js
@@ -1,22 +1,15 @@
 import { generateMedia } from 'styled-media-query';
 
 const colors = {
-  // brand: '#FFA500',
   brand: '#FFA500',
-  secondaryLight: '#3067A7',
   secondary: '#1d407f',
-  secondaryDark: '#1a325d',
-  border: '#dee0df',
+  border: '#d3d3d3',
   danger: '#f04134',
   success: '#42b56b',
   textDarkest: '#2d2d2d',
   textDark: '#525458',
   textLight: '#a6a7a9',
-  // offWhite: '#f8f8f8',
-  offWhite: '#F4F5F7',
-  darkOffWhite: '#ecedf2',
-  primaryDark: '#f6820c',
-  primaryLight: '#FFD17B',
+  offWhite: '#f8f8f8',
   // alias
   get info() { return this.secondary },
   get error() { return this.danger },
@@ -33,11 +26,6 @@ const zIndex = {
   modal: 1500,
   modalBackdrop: 1400,
   popover: 1600
-}
-
-const boxShadow = {
-  small: 'rgba(23,43,99,0.26) 0 7px 42px',
-  large: 'rgba(29,29,31,0.15) 0 10px 60px'
 }
 
 const spaces = {
@@ -69,6 +57,5 @@ export default {
   zIndex,
   spaces,
   breakPoints,
-  boxShadow,
   media: generateMedia(breakPoints)
 }
