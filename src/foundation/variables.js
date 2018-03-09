7a3c5aa125f23b8f3d4ede08922e40359910c1d9
Nathan Marshall <nathan123ca@gmail.com>
2018-03-08 16:50:57 -0800

    fix

--- src/foundation/variables.js
+++ src/foundation/variables.js
@@ -1,15 +1,22 @@
 import { generateMedia } from 'styled-media-query';
 
 const colors = {
+  // brand: '#FFA500',
   brand: '#FFA500',
+  secondaryLight: '#3067A7',
   secondary: '#1d407f',
-  border: '#d3d3d3',
+  secondaryDark: '#1a325d',
+  border: '#dee0df',
   danger: '#f04134',
   success: '#42b56b',
   textDarkest: '#2d2d2d',
   textDark: '#525458',
   textLight: '#a6a7a9',
-  offWhite: '#f8f8f8',
+  // offWhite: '#f8f8f8',
+  offWhite: '#F4F5F7',
+  darkOffWhite: '#ecedf2',
+  primaryDark: '#f6820c',
+  primaryLight: '#FFD17B',
   // alias
   get info() { return this.secondary },
   get error() { return this.danger },
@@ -26,6 +33,11 @@ const zIndex = {
   modal: 1500,
   modalBackdrop: 1400,
   popover: 1600
+}
+
+const boxShadow = {
+  small: 'rgba(23,43,99,0.26) 0 7px 42px',
+  large: 'rgba(29,29,31,0.15) 0 10px 60px'
 }
 
 const spaces = {
@@ -57,5 +69,6 @@ export default {
   zIndex,
   spaces,
   breakPoints,
+  boxShadow,
   media: generateMedia(breakPoints)
 }
