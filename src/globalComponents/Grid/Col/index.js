1e6c5ae6121002b4333bcfa30852a02423651eed
Bo Xing <xingbo828@gmail.com>
2018-06-07 16:59:46 -0700

    revert grid container update

--- src/globalComponents/Grid/Col/index.js
+++ src/globalComponents/Grid/Col/index.js
@@ -27,7 +27,6 @@ const calculateDisplay = (ratio) => (!isUndefined(ratio) && ratio === 0) && 'dis
 
 export default styled.div`
   padding: 0 1.5rem;
-  overflow: hidden;
 
   ${props => `
     ${calculateDisplay(props.xs)};
