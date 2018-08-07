040abf5500e31131f1a383db15ea819f47cd3b2c
Nathan Marshall <nathan123ca@gmail.com>
2018-08-06 18:21:40 -0700

    new styles for confirmation page. Copy still TBD

--- src/modules/Mover/Configurations/Success.js
+++ src/modules/Mover/Configurations/Success.js
@@ -1,51 +1,85 @@
 import React from 'react';
 import AnimatedIcon from '../../../globalComponents/AnimatedIcon';
 import Grid from '../../../globalComponents/Grid';
-import styled from 'styled-components';
+import Card from '../../../globalComponents/Card';
+import Box from '../../../globalComponents/Box';
+import Styled from 'styled-components';
 
 import { Paragraph, Heading } from '../../../globalComponents/Typography';
+import { Button } from '../../../globalComponents/Form';
 
-const SuccessContainer = styled.div`
+export const Header = Styled.div`
+  position: relative;
+  width: 100%;
+  height: 150px;
+  margin-bottom: -100px;
+  background: ${props=>props.theme.colors.success};
+
+  ${props=>props.theme.media.greaterThan('md')`
+    height: 300px;
+    margin-bottom: -175px;
+  `}
+
+  * {
+    z-index: 1;
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 120%;
+    padding-bottom: 50%;
+    background: white;
+    border-top-right-radius: 99rem;
+    border-top-left-radius: 99rem;
+    left: calc(-10%);
+    transform-origin: 50% 100%;
+    transform: scaleY(.1);
+    bottom:0;
+    z-index: 0;
+  }
+`
+
+export const Content = Styled.div`
   text-align: center;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  padding: 3rem 0;
+`
 
-  ${props => props.theme.media.greaterThan('md')`
-    padding: 4rem 0;
-  `};
-`;
-
-const Content = styled.div`
-  margin: 2rem 0;
-`;
-
-const { Container, Row, Col } = Grid;
 const { Checkmark } = AnimatedIcon;
 
 const ConfigurationSuccess = () => {
   return (
-    <Container>
-      <Row>
-        <Col lg="12" lgOffset="6" md="12" mdOffset="6" sm="24" xs="24">
-          <SuccessContainer>
-            <Checkmark style={{ width: 150, display: 'block' }} />
-            <Content>
-              <Heading wrapperTag="h1" size="sm">
-                Looks good! Your request has been sent.
-              </Heading>
-              <Paragraph>
-                It is a long established fact that a reader will be distracted
-                by the readable content of a page when looking at its layout.
-                The point of using Lorem Ipsum is that it has a more-or-less
-                normal distribution of letters.
-              </Paragraph>
-            </Content>
-          </SuccessContainer>
-        </Col>
-      </Row>
-    </Container>
+    <React.Fragment>
+      <Header />
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col xs={24} sm={24} mdOffset={4} md={16} lgOffset={5} lg={14} xlOffset={6} xl={12} >
+            <Card>
+              <Box inset={6}>
+                <Box vertical={6}>
+                  <Content>
+                    <Box between={6}>
+                      <div><Checkmark style={{ width: 125, display: 'block', margin: '0 auto' }} /></div>
+                      <Box between={4}>
+                        <Heading wrapperTag="h1" size="sm">
+                          We've sent your request
+                        </Heading>
+                        <Paragraph>
+                          It is a long established fact that a reader will be distracted
+                          by the readable content of a page when looking at its layout.
+                          The point of using Lorem Ipsum is that it has a more-or-less
+                          normal distribution of letters.
+                        </Paragraph>
+                      </Box>
+                      <div><Button secondary>Back to profile</Button></div>
+                    </Box>
+                  </Content>
+                </Box>
+              </Box>
+            </Card>
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </React.Fragment>
   );
 };
 
